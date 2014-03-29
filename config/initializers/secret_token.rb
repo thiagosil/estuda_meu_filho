4350e52d6fd3e3b200370c130676c07b74aa635a
Thiago Assis <thiagosil@gmail.com>
2014-03-29 08:32:43 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EstudaMeuFilho::Application.config.secret_key_base = 'a11ee4071292e4f34e4be7c73a5375f07aed8e9b60e04788e2c0b2f281de14a241ef7481c6ac32dcc6a5d1ccc49e9b4b2223f53afe91ca2aa11bea2eba88664f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EstudaMeuFilho::Application.config.session_store :cookie_store, key: '_estuda_meu_filho_session'
