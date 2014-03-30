f6d1826621080c12f313b45adcd835a1a991d43d
Thiago Assis <thiagosil@gmail.com>
2014-03-30 10:34:49 -0300

    Creating questions

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem "paperclip", "~> 4.1"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/migrate/20140327215148_create_questions.rb
+++ db/migrate/20140327215148_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string 
+      t.string :question
+      t.attachment :picture
+      
       t.timestamps
     end
   end
