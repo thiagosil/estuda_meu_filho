d546c331046895c53bd23081126fa87666f0fd0d
Thiago Assis <thiagosil@gmail.com>
2014-04-02 21:56:57 -0300

    Adding paperclip picture to question model.

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,9 @@ class Question < ActiveRecord::Base
   belongs_to :subject
   belongs_to :exam
 
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
   accepts_nested_attributes_for :answers, :allow_destroy => true
   accepts_nested_attributes_for :subject, :allow_destroy => true
   accepts_nested_attributes_for :exam, :allow_destroy => true
